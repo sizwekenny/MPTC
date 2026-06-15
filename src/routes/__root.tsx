e77ea6d961f80ca3597e6a19bbfac214b2672288
sizwekenny <sizwekenny36@gmail.com>
2026-06-15 11:27:44 +0200

    404 not found update on deploy

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -86,6 +86,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@Lovable" },
     ],
     links: [
+      { rel: "icon", href: "/favicon.ico" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
